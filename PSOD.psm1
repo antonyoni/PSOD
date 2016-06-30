43a22fa9e9ac562c1ece90498797fbd9b01c3249
antonyoni <a@arbo.red>
2016-06-30 23:37:03 +0100

    Adding module manifest and script files.

---
+++ PSOD.psm1
@@ -0,0 +1,15 @@
+################################################################################
+# Author     : Antony Onipko
+# Copyright  : (c) 2016 Antony Onipko. All rights reserved.
+################################################################################
+# This work is licensed under the Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+################################################################################
+
+Get-ChildItem -Path $psScriptRoot `
+    | ? { $_ -match '^Func_.+$' } `
+    | % {
+        . (Join-Path -Path $psScriptRoot -ChildPath $_)
+    }
