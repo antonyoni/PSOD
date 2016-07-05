21fac5f0534009c0f420a87c412f37dc023bff26
antonyoni <a@arbo.red>
2016-07-05 23:30:38 +0100

    Created Get-OneDriveItem functions. Needs testing - pipelining does not work correctly.

--- Get-OneDriveItem.ps1
+++ Get-OneDriveItem.ps1
@@ -1,26 +1,80 @@
-﻿
+﻿################################################################################
+# Author     : Antony Onipko
+# Copyright  : (c) 2016 Antony Onipko. All rights reserved.
+################################################################################
+# This work is licensed under the Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+################################################################################
+
+Function Get-OneDriveItem {
+    <#
+        .SYNOPSIS
+        
+        
+        .EXAMPLE
+        
+    #>
+    [CmdletBinding()]
+    [OutputType([PsObject])]
+    Param
+    (
+        # API resource path.
+        [Parameter(Mandatory=$True,
+                   Position=1,
+                   ValueFromPipeline=$True,
+                   ValueFromPipelineByPropertyName=$True)]
+        [string]$Path,
+
+        # The API authentication token.
+        [Parameter(Mandatory=$True,
+                   Position=2,
+                   ValueFromPipelineByPropertyName=$True)]
+        [OneDriveToken]$Token,
+
+        # The API path for the user's default drive's root. 'drive/root:/'. 
+        [Parameter(Mandatory=$False,
+                   ValueFromPipelineByPropertyName=$True)]
+        [string]$DriveRootPath = 'drive/root:/',
+
+        # Gets the items in the specified path, and all child items.
+        [Parameter(Mandatory=$False)]
+        [switch]$Recurse
+    )
+
+    Process {
+
+        Write-Verbose $Path
+        Write-Verbose $Token
+
+        $p = JoinPath $DriveRootPath $Path
+        $p = JoinPath $p ':/children'
+
+        Write-Verbose "Sending request to '$p'"
+
+        $rsp = Invoke-OneDriveApiCall -Path $p -Token $Token
+
+        if ($rsp.value) {
+            Write-Output $rsp.value
+        } else {
+            Write-Output $rsp
+        }
+
+        if ($Recurse) {
+            $rsp.value | ? { $_.folder.childCount -gt 0 } | % {
+                Get-OneDriveItem (JoinPath $Path $_.name) -Recurse
+            }
+        }
+
+    }
+
+}
+
+#Export-ModuleMember -Function 'Get-OneDriveItem'
 
 if ((Get-Date) -ge $token.ExpiryDT) {
     $token = Get-Content .\PSOD\onedrive.opt | Get-OneDriveAuthToken
 }
+$token | Get-OneDriveItem -Verbose
 
-#$rsp = $token | Invoke-OneDriveApiCall -Path 'drive/root:/:/children'
-#$rsp.value | select name, id, size, webUrl | Format-Table
-
-Function Get-OneDriveItem ($Path, [switch]$Recurse) {
-    $p = JoinPath 'drive/root:/' $Path
-    $p = JoinPath $p ':/children'
-    $rsp = $token | Invoke-OneDriveApiCall -Path $p
-    if ($rsp.value) {
-        Write-Output $rsp.value
-    } else {
-        Write-Output $rsp
-    }
-    if ($Recurse) {
-        $rsp.value | ? { $_.folder.childCount -gt 0 } | % {
-            Get-OneDriveItem (JoinPath $Path $_.name) -Recurse
-        }
-    }
-}
-
-Get-OneDriveItem -Recurse | select name, id, size, webUrl | Format-Table
