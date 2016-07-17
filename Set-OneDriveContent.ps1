e3e2726d0f152102949157d77894239533efdaa8
antonyoni <a@arbo.red>
2016-07-17 21:59:15 +0100

    Function working and tested. TODO: include a force switch and overwrite warning.

--- Set-OneDriveContent.ps1
+++ Set-OneDriveContent.ps1
@@ -14,13 +14,16 @@ Function Set-OneDriveContent {
         Uploads a new file, or update the contents of an existing OneDrive file.
         
         .EXAMPLE
-        
+        Set-OneDriveContent -Token $token -Path "Documents" -Source 'C:\Temp\test.pdf'
 
         .EXAMPLE
-        
+        Set-OneDriveContent -Token $token -Path "Documents/aNewDirectory/" -Source 'C:\Temp\test.pdf'
 
         .EXAMPLE
-        
+        $token | Set-OneDriveContent -Path "Documents/another-name.pdf" -Source 'C:\Temp\test.pdf'
+
+        .EXAMPLE
+        Set-OneDriveContent -Token $token -ItemId "85B75A4CE0397EE!1450" -Source 'C:\Temp\test.pdf'
     #>
     [CmdletBinding(DefaultParameterSetName='Item Path')]
     [Alias('odsc')]
@@ -70,12 +73,6 @@ Function Set-OneDriveContent {
         [string]$Source
     )
 
-    Begin {
-        if (!$Destination) {
-            $Destination = Get-Location
-        }
-    }
-
     Process {
 
         $upload = Get-Item -Path $Source
@@ -88,11 +85,22 @@ Function Set-OneDriveContent {
 
         if ($ItemId) {
             $remote = Get-OneDriveItem -Token $Token -ItemId $ItemId -ItemIdRoot $ItemIdRoot
+            if (!$remote) {
+                return
+            }
         } else {
-            $remote = Get-OneDriveItem -Token $Token -Path $Path -DriveRootPath $DriveRootPath
+            try {
+                $remote = Get-OneDriveItem -Token $Token -Path $Path -DriveRootPath $DriveRootPath
+            } catch {
+                # supress errors. OneDrive creates the full path if it doesn't exist.
+            }
         }
 
-        if ($remote.folder) {
+        if (!$remote -and !$Path.EndsWith('/')) {
+            # Assume path leaf is the file name even if no extension
+            $uploadPath = joinPath $DriveRootPath $Path
+            $uploadPath = $uploadPath.TrimEnd('/') + ':/content'
+        } elseif ($remote.folder -or $Path.EndsWith('/')) {
             if ($ItemId) {
                 $uploadPath = joinPath $ItemIdRoot $ItemId
                 $uploadPath = joinPath $uploadPath 'children'
@@ -101,53 +109,34 @@ Function Set-OneDriveContent {
             } else {
                 $uploadPath = joinPath $DriveRootPath $Path
                 $uploadPath = joinPath $uploadPath $upload.Name
-                $uploadPath = joinPath $uploadPath ':/content'
+                $uploadPath += ':/content'
             }
-            Write-Verbose "Destination path: $uploadPath"
-            Invoke-OneDriveApiCall -Token $Token `
-                                   -Path $uploadPath `
-                                   -Method PUT `
-                                   -InFile $upload.FullName
         } else {
-
-        }
-
-<#
-        if ($ItemId) {
-            $item = Get-OneDriveItem -Token $Token -ItemId $ItemId -ItemIdRoot $ItemIdRoot
-        } else {
-            $item = Get-OneDriveItem -Token $Token -Path $Path -DriveRootPath $DriveRootPath
-        }
-
-        if ($item.folder) {
-            $newDestination = Join-Path $Destination $item.name
-            New-Item -ItemType Directory -Path $newDestination | Out-Null
             if ($ItemId) {
-                $children = Get-OneDriveChildItem -Token $Token -ItemId $ItemId -ItemIdRoot $ItemIdRoot
+                $uploadPath = joinPath $ItemIdRoot $remote.parentReference.id
+                $uploadPath += ':'
+                $uploadPath = joinPath $uploadPath $remote.name 
+                $uploadPath += ':/content'
             } else {
-                $children = Get-OneDriveChildItem -Token $Token -Path $Path -DriveRootPath $DriveRootPath
+                $uploadPath = joinPath $DriveRootPath $Path
+                $uploadPath = $uploadPath.TrimEnd('/') + ':/content'
             }
-            $children | % {
-                Get-OneDriveContent -Token $Token `
-                                    -ItemId $_.id `
-                                    -ItemIdRoot $ItemIdRoot `
-                                    -Destination $newDestination
-            }
-        } else {
-            $outFile = Join-Path $Destination $item.name
-            $dloadPath = joinPath $ItemIdRoot $item.id
-            $dloadPath = joinPath $dloadPath 'content'
-            Invoke-OneDriveApiCall -Token $Token -Path $dloadPath -OutFile $outFile
         }
-#>
+
+        Write-Verbose "Destination path: $uploadPath"
+        
+        Invoke-OneDriveApiCall -Token $Token `
+                               -Path $uploadPath `
+                               -Method PUT `
+                               -InFile $upload.FullName
 
     }
 
 }
 
-#Export-ModuleMember -Function 'Set-OneDriveContent' -Alias 'odsc'
+Export-ModuleMember -Function 'Set-OneDriveContent' -Alias 'odsc'
 
-#<#
+<#
 Import-Module ..\PSOD
 Function joinPath($Path1, $Path2) {
     if (!$Path1) { $Path1 = "" }
@@ -158,13 +147,19 @@ if ((Get-Date) -ge $token.ExpiryDate) {
     $token = Get-Content .\onedrive.opt | Get-OneDriveAuthToken
 }
 #>
-#<#
+<#
 cd "C:\Temp"
 Set-OneDriveContent -Token $token -Path "temp" -Source 'C:\Temp\test1.pdf' -Verbose
-<#
-Set-OneDriveContent -Token $token -Path "Documents\Office Lens\28062016 2149 Office Lens.pdf" -Destination "c:\temp"
-Set-OneDriveContent -Token $token -ItemId "85B75A4CE0397EE!1399" -Verbose
-$token | Set-OneDriveContent -ItemId '85B75A4CE0397EE!1436' -Verbose
-Set-OneDriveContent -Token $token -Path "Documents"
+Set-OneDriveContent -Token $token -Path "temp/another1" -Source 'C:\Temp\test1.pdf' -Verbose
+Set-OneDriveContent -Token $token -Path "temp/another2/" -Source 'C:\Temp\test1.pdf' -Verbose
+Set-OneDriveContent -Token $token -Path "temp/temp.pdf" -Source 'C:\Temp\test1.pdf' -Verbose
+#upload by id of destination directory:
+Set-OneDriveContent -Token $token -ItemId "85B75A4CE0397EE!1450" -Source 'C:\Temp\test3.pdf' -Verbose
+#upload by id of destination file:
+$etagbf = ($token | Get-OneDriveItem -id '85B75A4CE0397EE!1451').etag
+Set-OneDriveContent -Token $token -ItemId "85B75A4CE0397EE!1451" -Source 'C:\Temp\test1.pdf' -Verbose
+$etagaf = ($token | Get-OneDriveItem -id '85B75A4CE0397EE!1451').etag
+$etagbf
+$etagaf
+$etagbf -ne $etagaf
 #>
-$brk
