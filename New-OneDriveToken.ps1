de44190d490e733083ef3bbd232f46fee8ec2a0c
antonyoni <a@arbo.red>
2016-07-19 22:18:51 +0100

    Add function to create a PSOD.OneDriveToken object Includes type extension XML.

---
+++ New-OneDriveToken.ps1
@@ -0,0 +1,104 @@
+################################################################################
+# Author     : Antony Onipko
+# Copyright  : (c) 2016 Antony Onipko. All rights reserved.
+################################################################################
+# This work is licensed under the Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+################################################################################
+
+Function New-OneDriveToken {
+    <#
+        .SYNOPSIS
+        Creates a new OneDrive Authentication token from an API response.
+        
+        .EXAMPLE
+        New-OneDriveToken -ResponseUrl $ApiResponse
+    #>
+    [CmdletBinding(DefaultParameterSetName='API Response')]
+    [Alias('odnt')]
+    [OutputType([PsObject])]
+    Param
+    (
+        # The API response URL from an authentication request.
+        [Parameter(Mandatory=$True,
+                   ValueFromPipeline=$True,
+                   Position=1,
+                   ParameterSetName='API Response')]
+        [string]$ResponseUrl,
+
+        # The access token.
+        [Parameter(Mandatory=$True,
+                   Position=1,
+                   ValueFromPipelineByPropertyName=$True,
+                   ParameterSetName='Token Details')]
+        [string]$AccessToken,
+
+        # Type of token requested. Either bearer or code.
+        [Parameter(Mandatory=$True,
+                   Position=2,
+                   ValueFromPipelineByPropertyName=$True,
+                   ParameterSetName='Token Details')]
+        [Alias("TokenType")]
+        [string]$Type,
+
+        # Number of seconds that the token is valid for.
+        [Parameter(Mandatory=$True,
+                   Position=3,
+                   ValueFromPipelineByPropertyName=$True,
+                   ParameterSetName='Token Details')]
+        [int]$ExpiresIn,
+
+        # The requested authentication scope.
+        [Parameter(Mandatory=$True,
+                   Position=4,
+                   ValueFromPipelineByPropertyName=$True,
+                   ParameterSetName='Token Details')]
+        [string]$Scope,
+
+        # The user ID of the requestor.
+        [Parameter(Mandatory=$True,
+                   Position=5,
+                   ValueFromPipelineByPropertyName=$True,
+                   ParameterSetName='Token Details')]
+        [string]$UserId
+    )
+
+    Process {
+
+        if ($ResponseUrl) {
+            $AccessToken = [regex]::Match($ResponseUrl, "access_token=(.+?)&").Groups[1].Value
+            $Type        = [regex]::Match($ResponseUrl, "token_type=(.+?)&").Groups[1].Value
+            $ExpiresIn   = [regex]::Match($ResponseUrl, "expires_in=(.+?)&").Groups[1].Value
+            $Scope       = [regex]::Match($ResponseUrl, "scope=(.+?)&").Groups[1].Value
+            $UserId      = [regex]::Match($ResponseUrl, "user_id=(.+?)(&|$)").Groups[1].Value
+        }
+
+        $nt = [pscustomobject]@{
+            AccessToken = $AccessToken
+            Type        = $Type
+            ExpiresIn   = $ExpiresIn
+            Created     = Get-Date
+            Scope       = $Scope
+            UserId      = $UserId
+        }
+
+        $nt.PsObject.TypeNames.Insert(0, "PSOD.OneDriveToken")
+        
+        Write-Output $nt
+    }
+
+}
+
+Export-ModuleMember -Function 'New-OneDriveToken' -Alias 'odnt'
+
+<#
+Update-TypeData -PrependPath .\PSOD.ps1xml
+$rsp = "http://localhost:8080/#access_token=ABC123==&token_type=bearer&expires_in=3600&scope=onedrive.readwrite files.read&user_id=uid123"
+$token = New-OneDriveToken $rsp
+$token
+$token | Get-Member | Format-Table
+$rsp | New-OneDriveToken
+New-OneDriveToken -AccessToken "abc123" -Type "type" -ExpiresIn 1000 -Scope "tiny" -UserId "uid"
+#>
