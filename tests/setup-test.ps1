14fa1d9c146cf6b236d53fb41ab42eb87280776f
antonyoni <a@arbo.red>
2016-09-03 18:23:47 +0100

    Adding remove-module to test setup.

--- tests/setup-test.ps1
+++ tests/setup-test.ps1
@@ -1,5 +1,7 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $modulePath = Split-Path $here -Parent
 
+if (Get-Module PSOD) { Remove-Module PSOD }
+
 Import-Module $modulePath
 . (Join-Path $modulePath PSOD.helpers.ps1)
